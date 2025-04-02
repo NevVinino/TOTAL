c002f9b99728034cca662b3a3c1473cd6cd702aa
Steven <banuser072@gmail.com>
2025-04-02 18:43:40 -0500

    Delete Vet directory

--- Vet/boton.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('nav ul');
-
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-    });
-});
